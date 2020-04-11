8e8afa8079c40da31f5164246c691bf41d59d40e
shiqingsheng <shi_work@126.com>
2020-04-11 12:25:08 +0800

    增加脚本

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Config
-LNMP环境配置信息
+LNMP环境配置信息<br />
 
-2020-04-11
-增加LNMP环境docker-compose.yml文件
-增加Nginx的配置文件
+2020-04-11<br />
+增加LNMP环境docker-compose.yml文件<br />
+增加Nginx的配置文件<br />
---
+++ deploy.sh
@@ -0,0 +1,62 @@
+#!bin/bash
+
+if [[ -n $1 ]]; then
+    work_dir=/docker
+    cd $work_dir
+
+    #启动容器
+    if [[$1 = "start"]]; then
+        sudo docker-compose up -d
+    fi
+    #停止容器
+    if [[$1 = "stop"]]; then
+        sudo docker-compose stop
+    fi
+    #重启容器
+    if [[$1 = "restart"]]; then
+        sudo docker-compose restart 
+    fi
+    exit 1
+fi
+
+read -t 30 -p "Git,Docker and Docker-Compose installed? [y/N]" result
+if [ $result != "y" ]; then
+    echo "Installation interruption"
+    exit 1;
+fi
+
+work_dir=/
+cd $work_dir
+
+if [ ! -d /docker/ ]; then
+    sudo mkdir -p docker/{nginx/{vhost,conf,log},wwwroot}
+fi
+
+work_dir=/docker
+cd $work_dir
+
+if [ ! -f nginx/conf/nginx.conf ]; then
+    if [ ! -d Config ]; then
+        echo "Start downloading profile..."
+        sudo git clone https://github.com/Shi-Mr/Config.git
+        echo "Profile download succeeded..."
+    fi
+    echo "Start copying profile..."
+    sudo mv Config/Nginx/Conf/nginx.conf ./nginx/conf
+    sudo mv Config/Nginx/Conf/default.conf ./nginx/vhost
+    sudo mv Config/Docker-Compose/docker-compose.yml ./
+    sudo mv Config/Nginx/Html/index.html ./wwwroot
+    echo "Profile copy successfully..."
+    sudo rm -rf Config
+fi
+
+sudo docker-compose up -d
+
+
+
+
+
+
+
+
+
